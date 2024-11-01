c9d65abe3c50fc9981d1377fcd0c6ea21c4a7b2e
JuHeonHa <hjhjaueon97@gmail.com>
2024-11-01 17:36:44 +0900

    feat: openAI API 연동

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 import express from "express";
 import OpenAI from "openai";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const client = new OpenAI({
@@ -14,12 +16,26 @@ const client = new OpenAI({
 // ChatGPT API 엔드포인트
 app.post("/api/chat", async (req, res) => {
   try {
-    const { message } = req.body;
+    const { imageData } = req.body;
 
     const chatCompletion = await client.chat.completions.create({
-      messages: [{ role: "user", content: message }],
+      messages: [
+        {
+          role: "user",
+          content: [
+            { type: "text", text: "What’s in this image?" },
+            {
+              type: "image_url",
+              image_url: {
+                url: imageData,
+              },
+            },
+          ],
+        },
+      ],
       model: "gpt-4o-mini",
     });
+    console.log(chatCompletion.choices[0]);
 
     res.json({
       response: chatCompletion.choices[0].message.content,
